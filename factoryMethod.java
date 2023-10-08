cf412f390a7ffe99fad7cee84a5cdbb01a19ef16
anely0o <ramyun.oppar@gmail.com>
2023-10-08 19:00:03 +0600

    2

--- factoryMethod.java
+++ factoryMethod.java
@@ -1,5 +1,42 @@
+import java.util.Random;
+
 public class factoryMethod {
     interface Animal {
         void create();
+    }
+    class Cat implements Animal {
+        public void create() {
+            System.out.println("Creating a Cat!meow");
+        }
+    }
+    class Dog implements Animal {
+        public void create() {
+            System.out.println("Creating a Dog!bark");
+        }
+    }
+    abstract class AnimalFactory {
+        abstract Animal createAnimal();
+
+        void operation() {
+            Animal animal = createAnimal();
+            animal.create();
+        }
+    }
+    class RandomAnimalFactory extends AnimalFactory {
+
+        @Override
+        Animal createAnimal() {
+           Random random = new Random();
+           if (random.nextBoolean()) {
+               return new Cat();
+           } else {
+               return new Dog();
+           }
+        }
+    }
+
+    public void main(String[] args) {
+        AnimalFactory animalFactory = new RandomAnimalFactory();
+        animalFactory.operation();
     }
 }
